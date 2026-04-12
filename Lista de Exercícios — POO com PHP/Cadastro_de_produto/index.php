a963738d3eeb12486725aeb8e0c207de046d841f
DamascenoGG <124640810+DamascenoGG@users.noreply.github.com>
2026-04-12 19:36:04 -0300

    Update index.php

--- Lista de Exercícios — POO com PHP/Cadastro_de_produto/index.php
+++ Lista de Exercícios — POO com PHP/Cadastro_de_produto/index.php
@@ -1,15 +1,11 @@
 <?php
 
-// 1. Incluindo a classe
 require_once 'Produto.php';
 
-// 2. Instanciando o objeto Produto com os valores do exemplo esperado
 $produto = new Produto("Teclado", 120.00, 3);
 
-// 3. Chamando o método para exibir os dados do produto
 $produto->mostrarDados();
 
-// 4. Chamando o método para calcular o estoque e exibindo o resultado formatado
 $valorEstoque = $produto->calcularValorEstoque();
 echo "Valor em estoque: R$ " . number_format($valorEstoque, 2, ',', '.') . "<br>";
 
